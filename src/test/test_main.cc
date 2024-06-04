3e8a5925f5df81700e9008b0c5329af6958d344c
nyashiki <tnakayashiki01@gmail.com>
2024-06-04 22:52:38 +0900

    Fix: printf format

--- src/test/test_main.cc
+++ src/test/test_main.cc
@@ -1,12 +1,13 @@
 #include "common.h"
 #include "../core/initializer.h"
+#include <cinttypes>
 
 int main() {
     nshogi::core::initializer::initializeAll();
     const auto Statistics = nshogi::test::common::TestBody::getInstance().run();
 
-    std::printf("Ran %llu test cases: ", Statistics.NumSuccess + Statistics.NumFail);
-    std::printf("%llu succeeded, %llu failed.\n", Statistics.NumSuccess, Statistics.NumFail);
+    std::printf("Ran %" PRIu64 " test cases: ", Statistics.NumSuccess + Statistics.NumFail);
+    std::printf("%" PRIu64 " succeeded, %" PRIu64 " failed.\n", Statistics.NumSuccess, Statistics.NumFail);
 
     return Statistics.NumFail != 0;
 }
