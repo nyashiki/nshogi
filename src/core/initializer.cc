43dcdd4fc5423743b748db5b9a17f14f4ab7c05c
nyashiki <tnakayashiki01@gmail.com>
2025-07-11 23:13:53 +0900

    Add: add mutex in initializeAll()

--- src/core/initializer.cc
+++ src/core/initializer.cc
@@ -10,20 +10,28 @@
 #include "initializer.h"
 #include "internal/bitboard.h"
 
+#include <mutex>
+
 namespace nshogi {
 namespace core {
 namespace initializer {
 
 void initializeAll() {
+    static std::mutex Mutex;
+    // Enter the critical section here to ensure that multiple threads
+    // cannot call initializeAll() concurrently.
+    // This can occur, for example, when `cargo test`
+    // calls this C++ code through the Rust API.
+    std::lock_guard<std::mutex> Lock(Mutex);
+
     static bool IsCalled = false;
 
     if (IsCalled) {
         return;
     }
 
-    IsCalled = true;
-
     internal::bitboard::initializeBitboards();
+    IsCalled = true;
 }
 
 } // namespace initializer
