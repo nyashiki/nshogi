3de88097d5437abef99ed1b0ec4b51cf6ec86e79
nyashiki <nyashiki@users.noreply.github.com>
2026-01-02 14:34:38 +0000

    deploy: 56b92736610fbac2cd54d1159e86ec0d28f27524

--- classnshogi_1_1solver_1_1dfpn_1_1Solver.js
+++ classnshogi_1_1solver_1_1dfpn_1_1Solver.js
@@ -3,6 +3,6 @@ var classnshogi_1_1solver_1_1dfpn_1_1Solver =
     [ "Solver", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#a916fb10f6f3a9ab395c94fc02dbbfe2e", null ],
     [ "~Solver", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#afb34b49293b59b420fea82cd83261009", null ],
     [ "searchedNodeCount", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#a9fbe162b5e47e08d6214b194533184bd", null ],
-    [ "solve", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#a3a853ed2a6ac8675318b2986e8afe092", null ],
-    [ "solveWithPV", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#aa6e048e815fafa990287987b18e33628", null ]
+    [ "solve", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#a3473347771d3c521e49ebe3cacd1d63e", null ],
+    [ "solveWithPV", "classnshogi_1_1solver_1_1dfpn_1_1Solver.html#ae00798c6ccfcd3111c68ea38f1c6d0b5", null ]
 ];
